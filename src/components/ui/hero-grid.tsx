9416135777a855323234c2c82a0e49dd43b1e9dc
Kevin <kevin.gouyet@protonmail.com>
2024-10-20 17:04:26 +0800

    add: right image

--- src/components/ui/hero-grid.tsx
+++ src/components/ui/hero-grid.tsx
@@ -1,26 +1,36 @@
+import Image from "next/image"
+
 export default function HeroGrid() {
     return (
         <div className="flex flex-row items-center gap-7 lg:mr-14 md:-mr-0 -mr-96 md:mt-0 mt-7 overflow-hidden flex-shrink-0">
-            <img
-                alt=""
-                src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+            <Image
+                alt="Le chef Lignac prépare des gourmandises"
+                src="/illustration-1.webp"
+                width={176}
+                height={264}
                 className="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-[176px] h-[264px]"
             />
             <div className="flex flex-col gap-7 flex-shrink-0">
-                <img
-                    alt=""
-                    src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+                <Image
+                    alt="Plein de tartines à découvrir"
+                    src="/illustration-2.webp"
+                    width={176}
+                    height={264}
                     className="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-[176px] h-[264px]"
-                    />
-                <img
-                    alt=""
-                    src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+                />
+                <Image
+                    alt="Des baguettes excellentes"
+                    src="/illustration-3.webp"
+                    width={176}
+                    height={264}
                     className="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-[176px] h-[264px]"
-                    />
+                />
             </div>
-            <img
-                alt=""
-                src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+            <Image
+                alt="Un croissant avec du chocolat"
+                src="/illustration-4.webp"
+                width={176}
+                height={264}
                 className="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-[176px] h-[264px]"
             />
         </div>
