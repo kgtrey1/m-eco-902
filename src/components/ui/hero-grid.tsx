9ca730cc92c94113af2d3e2cb4742007a95e5ff4
Kevin <kevin.gouyet@protonmail.com>
2024-10-20 13:11:13 +0800

    add: hero

---
+++ src/components/ui/hero-grid.tsx
@@ -0,0 +1,28 @@
+export default function HeroGrid() {
+    return (
+        <div className="flex flex-row items-center gap-7 lg:mr-14 md:-mr-0 -mr-96 md:mt-0 mt-7 overflow-hidden flex-shrink-0">
+            <img
+                alt=""
+                src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+                className="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-[176px] h-[264px]"
+            />
+            <div className="flex flex-col gap-7 flex-shrink-0">
+                <img
+                    alt=""
+                    src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+                    className="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-[176px] h-[264px]"
+                    />
+                <img
+                    alt=""
+                    src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+                    className="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-[176px] h-[264px]"
+                    />
+            </div>
+            <img
+                alt=""
+                src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+                className="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-[176px] h-[264px]"
+            />
+        </div>
+    )
+}
