389d2cd921deefc676100d0f70a881f8a2d6c827
Kevin <kevin.gouyet@protonmail.com>
2024-10-25 18:49:16 +0800

    update: page title and semantic of landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,17 @@ import Location from '@/components/ui/location'
 import ContactForm from '@/components/ui/contact-form'
 
 export default function Home() {
-  return (
-      <div>
-          <div className="md:h-screen w-full isolate overflow-hidden flex flex-col">
-              { /* insert header here */ }
-              <NewHero/>
-          </div>
-          <Biography />
-          <Products />
-          { /* insert "On parle de nous here" */ }
-          <Location />
-          <ContactForm />
-      </div>
-  )
+    return (
+        <main>
+            <div className="md:h-screen w-full isolate overflow-hidden flex flex-col">
+                { /* insert header here */ }
+                <NewHero/>
+            </div>
+            <Biography />
+            <Products />
+            { /* insert "On parle de nous here" */ }
+            <Location />
+            <ContactForm />
+        </main>
+    )
 }
