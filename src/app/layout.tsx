fd3d065baa552ae125ea5c19b22642311eb7f547
Kevin <kevin.gouyet@protonmail.com>
2024-10-26 22:39:45 +0800

    del: fonts, add: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,6 @@
 import type { Metadata } from 'next'
-import { Roboto } from 'next/font/google'
 
 import './globals.css'
- 
-const roboto = Roboto({
-    weight: ['400', '500', '700'],
-    subsets: ['latin'],
-})
 
 export const metadata: Metadata = {
     title: "Philippe Lugnac - Maître pâtissier",
@@ -16,7 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="fr">
-            <body className={`${roboto.className} antialiased max-h-screen bg-pastry`}>
+            <body className={`antialiased max-h-screen bg-pastry`}>
                 {children}
             </body>
         </html>
