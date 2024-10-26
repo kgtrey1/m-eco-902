e0ef1be24502559e188a7e20fbbc20d236179518
Kevin <kevin.gouyet@protonmail.com>
2024-10-27 00:57:15 +0800

    hotfix: margin on press

--- src/components/ui/press.tsx
+++ src/components/ui/press.tsx
@@ -7,12 +7,12 @@ export default function PressSection() {
             <p className="mb-8 text-lg text-gray-600">
                 Nos dernières parutions dans la presse et sur internet.
             </p>
-            <div className="flex justify-center items-center space-x-8">
+            <div className="flex justify-center items-center space-x-8 overflow-hidden">
                 <Image src="/logo-elle.webp" alt="ELLE" width={102} height={45  } />
                 <Image src="/logo-lemonde.webp" alt="Le Monde" width={204} height={45} />
                 <Image src="/logo-vogue.webp" alt="Vogue" width={106} height={45} />
-                <Image src="/logo-cntraveler.webp" alt="Traveler" width={119} height={45} />
-                <Image src="/fou-patesserie.webp" alt="Fou de Pâtisserie" width={193} height={45} />
+                <Image src="/logo-cntraveler.webp" alt="Traveler" width={119} height={45} className='md:block hidden' />
+                <Image src="/fou-patesserie.webp" alt="Fou de Pâtisserie" width={193} height={45} className='md:block hidden' />
             </div>
         </div>
     )
