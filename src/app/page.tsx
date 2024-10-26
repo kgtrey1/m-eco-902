3e6edb975832bea84f87c2faa389cac05f9b1877
Kevin <kevin.gouyet@protonmail.com>
2024-10-26 23:23:04 +0800

    opti: image press

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Biography from '@/components/ui/biography'
 import Products from '@/components/ui/products'
 import Location from '@/components/ui/location'
 import ContactForm from '@/components/ui/contact-form'
-import PressSection from '@/components/ui/On-parle'
+import PressSection from '@/components/ui/press'
 import Footer from '@/components/ui/footer'
 
 export default function Page() {
---
+++ src/components/ui/press.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+
+export default function PressSection() {
+    return (
+        <div className="bg-bg-chief py-12 text-center">
+            <h2 className="text-2xl font-bold mb-4">On parle de nous</h2>
+            <p className="mb-8 text-lg text-gray-600">
+                Nos dernières parutions dans la presse et sur internet.
+            </p>
+            <div className="flex justify-center items-center space-x-8">
+                <Image src="/logo-elle.webp" alt="ELLE" width={102} height={45  } />
+                <Image src="/logo-lemonde.webp" alt="Le Monde" width={204} height={45} />
+                <Image src="/logo-vogue.webp" alt="Vogue" width={106} height={45} />
+                <Image src="/logo-cntraveler.webp" alt="Traveler" width={119} height={45} />
+                <Image src="/fou-patesserie.webp" alt="Fou de Pâtisserie" width={193} height={45} />
+            </div>
+        </div>
+    )
+}
